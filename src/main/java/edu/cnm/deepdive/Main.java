5ffb9597a933414c82fc98531b3e732414b23a56
Cathy Bastareaud <catybastareaud@gmail.com>
2022-02-03 14:58:50 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO: Read command line arguments for game difficulty.
+    // TODO: Request (POST) start of new game from service.
+    // TODO: While the game is not completed:
+    //   - get user's guess.
+    //   - Send guess (POST) to service.
+    //   - Display matches ( from service response) to user.
+    // TODO: Display statistic (# of guesses. time to solve) to user.
+    
+
+
+  }
+
+}
