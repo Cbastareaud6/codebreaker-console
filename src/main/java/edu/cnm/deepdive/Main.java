908e76dee8294f20e2c3a44db3c7ce5ba325ce66
Cathy Bastareaud <catybastareaud@gmail.com>
2022-02-07 10:26:52 -0700

    Refactored for internationalization.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -2,13 +2,17 @@ package edu.cnm.deepdive;
 
 import edu.cnm.deepdive.controller.Player;
 import java.io.IOException;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Main {
 
 
   public static void main(String[] args) throws IOException {
     // TODO: Read command line arguments for game difficulty.
-    Player player = new Player(System.in, System.out);
+    Scanner scanner = new Scanner(System.in);
+    ResourceBundle bundle = ResourceBundle.getBundle("Strings");
+    Player player = new Player( scanner, System.out, bundle);
     player.play("ABCDEF", 3);
 
 
